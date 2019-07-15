3b2fa65fb2ee0510629019ac5d657defa0809992
Austin Leung <austinle@andrew.cmu.edu>
2019-07-15 16:56:00 -0400

    fixed navbar text

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  document.getElementById("welcome").innerHTML = 'Welcome back, ' + profile.getName();
+  document.getElementById("welcome").innerHTML = 'Hi, ' + profile.getName();
 }
 
 function signOut() {
