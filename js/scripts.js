f158d3e70b2421c01d66a1fdc7af41c343fbeec1
Austin Leung <austinle@andrew.cmu.edu>
2019-08-02 09:28:40 -0400

    Calendar/Signin to view lessons

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,13 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   document.getElementById("welcome").innerHTML = 'Hi, ' + profile.getName();
+  document.getElementById("calend").innerHTML = '''
+  <h3 class="mb-3 font-weight-normal"> Ready to start? Hop into a lesson listed below whenever convenient!</h2>
+    <br>
+    <iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=bWVicm9uYmFtZXNAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;title=Adagio%20Lesson%20Schedule&amp;showNav=1&amp;showDate=1&amp;showPrint=1&amp;showTabs=1&amp;showCalendars=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <br>
+    <br>
+  <h6 class="mb-3 font-weight-normal">Follow the Zoom meeting link in the description to join the lesson at the listed time.</h6>''';
 }
 
 function signOut() {
@@ -13,16 +20,5 @@ function signOut() {
     console.log('User signed out.');
   });
   document.getElementById("welcome").innerHTML = '';
+  document.getElementById("calend").innerHTML = '<h3 class="mb-3 font-weight-normal">You must sign in to view lessons.</h3>';
 }
-
-var Acuity = require('acuityscheduling');
-
-var acuity = Acuity.basic({
-  userId: 18048999,
-  apiKey: '4afe4805445385afe793ccdd3d5b5f78 '
-});
-
-acuity.request('appointments', function (err, res, appointments) {
-  if (err) return console.error(err);
-  console.log(appointments);
-});
